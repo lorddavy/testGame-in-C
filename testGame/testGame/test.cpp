6e0e7b74d4aaa5fe01de5617df7f2495beaf585b
David Pérez <dppjr3@gmail.com>
2016-07-13 19:41:13 +0200

    day 4

--- testGame/testGame/test.cpp
+++ testGame/testGame/test.cpp
@@ -6,12 +6,20 @@ void foo(void)	//Function signature or prototype
 	OutputDebugStringA(Foo);
 }
 
+
+struct projectile {
+	char unsigned IsThisOnFire;
+	int Damage;
+	int ParticlesPerSecond;
+	short HowManyCooKS;
+};
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
 	int       nCmdShow
-){
+) {
 	/*char SmallS; // 8 bits - 256 diff values: [-128,127]
 	char unsigned SmallU;  // 8 bits unsigned - 256 diff values: [0,255]
 
@@ -21,11 +29,39 @@ int CALLBACK WinMain(
 	int LargeS; // 32 bits - 4 billion diff values
 	int unsigned LargeU; // 32 bits unsigned - 4 billion diff values*/
 
-	
-	char unsigned Test;
-	char unsigned *TestPointer;
+	projectile Test;
 
-	TestPointer = &Test;
+	int size = sizeof(Test);
+
+	Test.IsThisOnFire = 1;
+	Test.Damage = 342314;
+	Test.ParticlesPerSecond = 14423;
+	Test.HowManyCooKS = 50;
+
+	//projectile *ProjectilePointer = &Test;
+	unsigned short *MrPointerMan = (unsigned short*)&Test;
+
+
+	projectile Projectiles[40];
+
+	projectile *ProjectilePointer = Projectiles;
+
+	int WhichProjectile;
+	WhichProjectile = 4;
+
+	Projectiles[WhichProjectile];
+
+	Projectiles[30].Damage = 60;
+
+	(ProjectilePointer+30)->Damage = 100;
+	char *BytePointer = (char*)ProjectilePointer;
+	((projectile*)(BytePointer + 30 * sizeof(projectile)))->Damage = 200;
+
+	/*char unsigned *TestPointer;
+
+	TestPointer = &Test;*/
+
+	//Test = 200;
 
 	/*
 	//Overflow
